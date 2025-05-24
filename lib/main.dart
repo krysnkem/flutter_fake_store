2c992fdddd6154cf6cca0ab10bb29d63b9a83fd8
Christian Onyisi <krysnkem@gmail.com>
2025-05-25 00:34:50 +0100

    add multi bloc provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_fake_store/core/routing/app_routes.dart';
 import 'package:flutter_fake_store/core/utils/theme/app_theme.dart';
+import 'package:flutter_fake_store/injection/injection.dart';
+import 'package:flutter_fake_store/presentation/blocs/auth/auth_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +15,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      routerConfig: AppRoutes.router,
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<AuthBloc>(),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: AppTheme.lightTheme,
+        routerConfig: AppRoutes.router,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
